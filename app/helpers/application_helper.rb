d2ff2eaaa70edf11b7b6e166c6ff6bb33e3e53cd
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-08-11 11:06:25 +0600

    md change to lg in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def nav_link_class(path)
-    base_classes = "nav-link block py-2 px-3 rounded md:bg-transparent md:p-0 md:hover:bg-transparent dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+    base_classes = "nav-link block py-2 px-3 rounded lg:bg-transparent lg:p-0 lg:hover:bg-transparent dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
     
     if current_page?(path)
       "#{base_classes} active text-foundationprimarygreen bg-gray-50"
@@ -10,6 +10,6 @@ module ApplicationHelper
   end
   
   def nav_link_class_static
-    "nav-link block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+    "nav-link block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
   end
 end
