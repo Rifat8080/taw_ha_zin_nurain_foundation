1cf36773ae1db24d4da203db82d12d081ca14a20
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-08-19 04:14:11 +0600

    3 cards

--- app/javascript/modules/carousel.js
+++ app/javascript/modules/carousel.js
@@ -10,7 +10,7 @@ document.addEventListener('turbo:load', function() {
     breakpoints: {
       640: { slidesPerView: 1 },
       768: { slidesPerView: 2 },
-      1024: { slidesPerView: 4 },
+      1024: { slidesPerView: 3 },
     },
     pagination: { el: '.swiper-pagination', clickable: true },
     navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
