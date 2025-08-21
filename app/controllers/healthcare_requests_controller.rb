05ee4a1fb01aeffdaa314e4d46749b4f91709613
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-08-21 12:38:34 +0600

    indentation

--- app/controllers/healthcare_requests_controller.rb
+++ app/controllers/healthcare_requests_controller.rb
@@ -1,5 +1,5 @@
 class HealthcareRequestsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [ :index, :show ]
   before_action :set_healthcare_request, only: [ :show, :edit, :update, :destroy, :approve, :reject, :complete ]
 
   def index
