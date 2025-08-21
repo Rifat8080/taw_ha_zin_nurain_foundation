1fec8522dae0ffee7f90d902ab000d917ecaff1a
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-08-21 23:38:44 +0600

    indent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,14 @@ module ApplicationHelper
   include NumberHelper
   def nav_link_class(path)
     base_classes = "nav-link block py-2 px-3 font-medium transition-colors duration-200 lg:bg-transparent lg:p-0 lg:hover:bg-transparent"
-    
+
     if current_page?(path)
       "#{base_classes} active text-foundationprimarygreen"
     else
       "#{base_classes} text-gray-700 hover:text-foundationprimarygreen dark:text-gray-400 dark:hover:text-white"
     end
   end
-  
+
   def nav_link_class_static
     "nav-link block py-2 px-3 font-medium text-gray-700 hover:text-foundationprimarygreen lg:bg-transparent lg:p-0 lg:hover:bg-transparent transition-colors duration-200 dark:text-gray-400 dark:hover:text-white"
   end
