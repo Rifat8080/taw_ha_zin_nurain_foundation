4efda5ce1e9a4db346ab8e6947b144c3a28de7e8
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-09-15 17:23:51 +0600

    volunteer team fixed

--- app/controllers/volunteers_teams_controller.rb
+++ app/controllers/volunteers_teams_controller.rb
@@ -1,5 +1,7 @@
 class VolunteersTeamsController < ApplicationController
   before_action :set_volunteers_team, only: [ :show, :edit, :update, :destroy ]
+  before_action :authorize_admin!, only: [:new, :create, :destroy]
+  before_action :authorize_manage_team, only: [:edit, :update]
 
   def index
   @volunteers_teams = VolunteersTeam.includes(:volunteers, :work_orders).all
@@ -42,6 +44,23 @@ class VolunteersTeamsController < ApplicationController
   def destroy
     @volunteers_team.destroy
     redirect_to volunteers_teams_url, notice: "Team was successfully deleted."
+  end
+
+  private
+
+  def authorize_manage_team
+    return if current_user && (current_user.role == 'admin')
+
+    volunteer = current_user.respond_to?(:volunteer) ? current_user.volunteer : nil
+    unless volunteer && volunteer.role == 'leader' && @volunteers_team.volunteers.exists?(id: volunteer.id)
+      redirect_to @volunteers_team, alert: 'You are not authorized to perform that action.'
+    end
+  end
+
+  def authorize_admin!
+    unless current_user&.role == 'admin'
+      redirect_to volunteers_teams_path, alert: 'You are not authorized to perform that action.'
+    end
   end
 
   private
--- app/helpers/volunteers_teams_helper.rb
+++ app/helpers/volunteers_teams_helper.rb
@@ -10,4 +10,17 @@ module VolunteersTeamsHelper
   def district_options
     VolunteersTeam.distinct.pluck(:district).compact.sort.map { |d| [ d, d ] }
   end
+  
+  def can_manage_team?(user, team)
+  return false unless user && team
+
+  # Admin users can always manage teams
+  return true if user.role == 'admin'
+
+  # Map the current_user to a Volunteer record and check leadership + membership
+  volunteer = user.respond_to?(:volunteer) ? user.volunteer : nil
+  return false unless volunteer
+
+  volunteer.role == 'leader' && team.volunteers.exists?(id: volunteer.id)
+  end
 end
